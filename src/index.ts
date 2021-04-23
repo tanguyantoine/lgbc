d5811df845d1a45e781d7316ee773bb96b5099f1
TANGUY Antoine <antoine.tanguy@sendinblue.com>
2021-04-23 15:25:13 +0200

    fix: delete branches

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,58 @@
-import * as Git from 'nodegit'
-import * as inquire from 'inquirer'
+import Git, { Reference } from 'nodegit'
+import * as inquirer from 'inquirer'
 import * as path from 'path'
 import yargs from 'yargs'
 
-const { cwd = process.cwd() } = yargs(process.argv.slice(2))
-  .options({
-    cwd: {
-      describe: 'current working directory',
-    },
-  })
-  .help().argv
+async function main() {
+  const { cwd } = getArgs()
+  try {
+    const repo = await Git.Repository.open(cwd)
+    const refs = await repo.getReferences()
+    const { branches } = await inquirer.prompt<{ branches: Reference[] }>([
+      {
+        name: 'branches',
+        type: 'checkbox',
+        choices: refs
+          .filter((r) => !r.isRemote() && !r.isTag())
+          .map((ref) => ({
+            value: ref,
+            name: ref.shorthand(),
+          })),
+      },
+    ])
+    branches.forEach((ref) => {
+      if (ref.delete() === 0) {
+        console.log('Deleted: ', ref.shorthand())
+      } else {
+        console.error('error while deleting: ', ref.shorthand())
+      }
+    })
+    main()
+  } catch (e) {
+    switch (e.errorFunction) {
+      case 'Repository.open': {
+        console.error('Not a valid git repository')
+        break
+      }
+      default: {
+        console.error(e.toString())
+      }
+    }
+  }
+}
 
-console.log(path.resolve(cwd as string))
+function getArgs() {
+  const { cwd = process.cwd() } = yargs(process.argv.slice(2))
+    .options({
+      cwd: {
+        describe: 'current working directory',
+      },
+    })
+    .help().argv
 
-console.log({ cwd })
+  return {
+    cwd: path.resolve(cwd as string),
+  }
+}
+
+main()
