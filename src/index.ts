37639edc0b25530639f56ecc9ae2d231c932f5a7
TANGUY Antoine (SIB) <tanguyantoine@users.noreply.github.com>
2022-11-29 12:31:14 +0100

    fix: migrate to simple git

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Git, { Reference } from 'nodegit'
+import { BranchSummary, BranchSummaryBranch, simpleGit } from 'simple-git'
 import * as inquirer from 'inquirer'
 import * as path from 'path'
 import yargs from 'yargs'
@@ -6,27 +6,22 @@ import yargs from 'yargs'
 async function main() {
   const { cwd } = getArgs()
   try {
-    const repo = await Git.Repository.open(cwd)
-    const refs = await repo.getReferences()
-    const { branches } = await inquirer.prompt<{ branches: Reference[] }>([
+    const git = simpleGit()
+    const refs = await git.branchLocal()
+    const { branches } = await inquirer.prompt<{ branches: BranchSummaryBranch[] }>([
       {
         name: 'branches',
         type: 'checkbox',
-        choices: refs
-          .filter((r) => !r.isRemote() && !r.isTag())
+        choices: refs.all
+          .filter((name) => !name.startsWith('remotes'))
           .map((ref) => ({
-            value: ref,
-            name: ref.shorthand(),
+            value: refs.branches[ref],
+            disabled: refs.branches[ref].current,
+            name: ref,
           })),
       },
     ])
-    branches.forEach((ref) => {
-      if (ref.delete() === 0) {
-        console.log('Deleted: ', ref.shorthand())
-      } else {
-        console.error('error while deleting: ', ref.shorthand())
-      }
-    })
+    git.deleteLocalBranches(branches.map(b => b.name))
     main()
   } catch (e) {
     switch (e.errorFunction) {
